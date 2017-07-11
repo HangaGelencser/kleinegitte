5384289c9c16de36c65f39329a2d9d9f977050b6
Balazs Pest <pest.balazs@gmail.com>
2017-07-11 11:13:54 +0200

    hellou

--- gitte.py
+++ gitte.py
@@ -1,13 +1,12 @@
 def hello_gitte():
     print("Hello,bitte I'm Gitte!")
 
-<<<<<<< HEAD
+
 
 def add(a, b):
     print (a+b)
 
 add(3, 5)   
-=======
+
 def joke():
     print("HaHa")
->>>>>>> a96fc6d6bce16261a5759a75763306e9c83819c8
